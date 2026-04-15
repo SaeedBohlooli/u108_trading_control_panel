3ac463229bfa633e11ba9c29b7ec5a15a5743185
Saeed Bohlooli <s.bohlooli@gmail.com>
2026-04-14 22:50:07 -0400

    reset

--- bin-bash/run_api.sh
+++ bin-bash/run_api.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # filepath: c:\Users\saeed\Documents\13-code-git\u108_trading_control_panel\bin-bash\run_api.sh
 cd "$(dirname "$0")/.."
+if [ ! -d .venv ]; then
+  python -m venv .venv
+fi
+source .venv/bin/activate
 pip install -r requirements.txt
 python api/control_panel_api.py
